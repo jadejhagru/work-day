2c62278a09736648dca36266bd1ad3be9c91481a
Jade Jhagru <j.jhagru@hotmail.com>
2021-03-23 13:33:23 -0400

    added jquery

--- script.js
+++ script.js
@@ -1,260 +1,95 @@
-window.onload = function(){
-    colorCode();
-    nineAM();
-    tenAM();
-    elevenAM();
-    twelvePM();
-    onePM();
-    twoPM();
-    threePM();
-    fourPM();
-    fivePM();
-   }
+$(document).ready(function() {
+  var events = [];
   
-  // Current date and time
+  // listen for save button clicks
+  $(".saveBtn").on("click", function() {
+    // get nearby values
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    var dateAdded = moment().format("dddd, MMMM Do");
+    var timeAdded = moment().format("h:mm:ss a");
+
+
+    events.push({description: value, time: time, date: dateAdded, time: timeAdded});
+
+    // save the value in localStorage as time
+    localStorage.setItem("events", JSON.stringify(events));
+    
+  });
+
+  function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      // check if we've moved past this time
+      // if the current hour is greater than the block hour
+      // then add class "past"
+      if(currentHour > blockHour) {
+        $(this).addClass("past");
+      }
+      // if they are equal
+      // then remove class "past" and add class "present"
+      else if(currentHour === blockHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+      // else
+      // remove class "past", remove class "present", add class "future"
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+      
+    });
+  }
+
+  hourUpdater();
+
+  // set up interval to check if current time needs to be updated
+  // which means execute hourUpdater function every 15 seconds
+  var secondsLeft = 15;
+  function setTime() {
+    setInterval(function() {
+      secondsLeft--;
   
-  var currentDay = moment().format("MMMM Do YYYY");
-  $("#currentDay").append(currentDay);
+      if(secondsLeft === 0) {
+        hourUpdater();
+        secondsLeft = 15;
+      }
   
-  var currentTime = moment().format("h:mm:ss a");
-  $("#currentTime").append(currentTime);
-  
- // Time blocks for the days
-  
-  var current = new Date().getHours();
-  
-  function colorCode() {
-    if (current > 9) {
-      $("#comment9am").addClass("past");
-    } else if (current >= 9 && current < 10) {
-      $("#comment9am").addClass("present");
-    } else if (current < 9) {
-      $("#comment9am").addClass("future");
-    }
-    if (current > 10) {
-      $("#comment10am").addClass("past");
-    } else if (current >= 10 && current < 11) {
-      $("#comment10am").addClass("present");
-    } else if (current < 10) {
-      $("#comment10am").addClass("future");
-    }
-    if (current > 11) {
-      $("#comment11am").addClass("past");
-    } else if (current >= 11 && current < 12) {
-      $("#comment11am").addClass("present");
-    } else if (current < 11) {
-      $("#comment11am").addClass("future");
-    }
-    if (current > 12) {
-      $("#comment12pm").addClass("past");
-    } else if (current >= 12 && current < 13) {
-      $("#comment12pm").addClass("present");
-    } else if (current < 12) {
-      $("#comment12pm").addClass("future");
-    }
-    if (current > 13) {
-      $("#comment1pm").addClass("past");
-    } else if (current >= 13 && current < 14) {
-      $("#comment1pm").addClass("present");
-    } else if (current < 13) {
-      $("#comment1pm").addClass("future");
-    }
-    if (current > 14) {
-      $("#comment2pm").addClass("past");
-    } else if (current >= 14 && current < 15) {
-      $("#comment2pm").addClass("present");
-    } else if (current < 14) {
-      $("#comment2pm").addClass("future");
-    }
-    if (current > 15) {
-      $("#comment3pm").addClass("past");
-    } else if (current >= 15 && current < 16) {
-      $("#comment3pm").addClass("present");
-    } else if (current < 15) {
-      $("#comment3pm").addClass("future");
-    }
-    if (current > 16) {
-      $("#comment4pm").addClass("past");
-    } else if (current >= 16 && current < 17) {
-      $("#comment4pm").addClass("present");
-    } else if (current < 16) {
-      $("#comment4pm").addClass("future");
-    }
-    if (current > 17) {
-      $("#comment5pm").addClass("past");
-    } else if (current >= 17 && current < 18) {
-      $("#comment5pm").addClass("present");
-    } else if (current < 17) {
-      $("#comment5pm").addClass("future");
+    }, 1000);
+  }
+  setTime();
+
+  // reset on new day
+  var currentDay = moment().format("dddd, MMMM Do");
+  for(var i = 0; i < events.length; i++) {
+    if(currentDay.isAfter(events[i].date)) {
+      events[i].description = "";
+      events[i].time = "";
+      events[i].date = "";
+      events.length = 0;
     }
   }
-  
-  
-  // WHEN I click into a timeblock
-  // THEN I can enter an event
-  // WHEN I click the save button for that timeblock
-  // THEN the text for that event is saved in local storage
-  // WHEN I refresh the page
-  // THEN the saved events persist
-  
-  function nineAM() {
-    var input_textarea = document.querySelector("#comment9am");
-    var output_div = document.querySelector("#comment9am");
-    var save_button = document.querySelector("#button9am");
-  
-    save_button.addEventListener("click", updateOutput);
-  
-    output_div.textContent = localStorage.getItem("content");
-    input_textarea.value = localStorage.getItem("content");
-  
-    function updateOutput() {
-      localStorage.setItem("content", input_textarea.value);
-  
-      output_div.textContent = input_textarea.value;
-    }
+
+  // load any saved data from localStorage
+  var storedEvents = JSON.parse(localStorage.getItem("events"));
+
+  if (storedEvents !== null) {
+    events = storedEvents;
   }
-  
-  
-  
-  function tenAM() {
-    var input_textarea2 = document.querySelector("#comment10am");
-    var output_div2 = document.querySelector("#comment10am");
-    var save_button2 = document.querySelector("#button10am");
-  
-    save_button2.addEventListener("click", updateOutput2);
-  
-    output_div2.textContent = localStorage.getItem("content2");
-    input_textarea2.value = localStorage.getItem("content2");
-  
-    function updateOutput2() {
-      localStorage.setItem("content2", input_textarea2.value);
-  
-      output_div2.textContent = input_textarea2.value;
-    }
+
+  for(var i = 0; i < events.length; i++) {
+    var userDescription = events[i].description;
+    $("#" + events[i].time).children(".description").text(userDescription);
   }
-  
-  
-  function elevenAM() {
-    var input_textarea3 = document.querySelector("#comment11am");
-    var output_div3 = document.querySelector("#comment11am");
-    var save_button3 = document.querySelector("#button11am");
-  
-    save_button3.addEventListener("click", updateOutput3);
-  
-    output_div3.textContent = localStorage.getItem("content3");
-    input_textarea3.value = localStorage.getItem("content3");
-  
-    function updateOutput3() {
-      localStorage.setItem("content3", input_textarea3.value);
-  
-      output_div3.textContent = input_textarea3.value;
-    }
-  }
-  
-  
-  function twelvePM() {
-    var input_textarea4 = document.querySelector("#comment12pm");
-    var output_div4 = document.querySelector("#comment12pm");
-    var save_button4 = document.querySelector("#button12pm");
-  
-    save_button4.addEventListener("click", updateOutput4);
-  
-    output_div4.textContent = localStorage.getItem("content4");
-    input_textarea4.value = localStorage.getItem("content4");
-  
-    function updateOutput4() {
-      localStorage.setItem("content4", input_textarea4.value);
-  
-      output_div4.textContent = input_textarea4.value;
-    }
-  }
-  
-  
-  function onePM() {
-    var input_textarea5 = document.querySelector("#comment1pm");
-    var output_div5 = document.querySelector("#comment1pm");
-    var save_button5 = document.querySelector("#button1pm");
-  
-    save_button5.addEventListener("click", updateOutput5);
-  
-    output_div5.textContent = localStorage.getItem("content5");
-    input_textarea5.value = localStorage.getItem("content5");
-  
-    function updateOutput5() {
-      localStorage.setItem("content5", input_textarea5.value);
-  
-      output_div5.textContent = input_textarea5.value;
-    }
-  }
-  
-  
-  function twoPM() {
-    var input_textarea6 = document.querySelector("#comment2pm");
-    var output_div6 = document.querySelector("#comment2pm");
-    var save_button6 = document.querySelector("#button2pm");
-  
-    save_button6.addEventListener("click", updateOutput6);
-  
-    output_div6.textContent = localStorage.getItem("content6");
-    input_textarea6.value = localStorage.getItem("content6");
-  
-    function updateOutput6() {
-      localStorage.setItem("content6", input_textarea6.value);
-  
-      output_div6.textContent = input_textarea6.value;
-    }
-  }
-  
-  
-  function threePM() {
-    var input_textarea7 = document.querySelector("#comment3pm");
-    var output_div7 = document.querySelector("#comment3pm");
-    var save_button7 = document.querySelector("#button3pm");
-  
-    save_button7.addEventListener("click", updateOutput7);
-  
-    output_div7.textContent = localStorage.getItem("content7");
-    input_textarea7.value = localStorage.getItem("content7");
-  
-    function updateOutput7() {
-      localStorage.setItem("content7", input_textarea7.value);
-  
-      output_div7.textContent = input_textarea7.value;
-    }
-  }
-  
-  
-  function fourPM() {
-    var input_textarea8 = document.querySelector("#comment4pm");
-    var output_div8 = document.querySelector("#comment4pm");
-    var save_button8 = document.querySelector("#button4pm");
-  
-    save_button8.addEventListener("click", updateOutput8);
-  
-    output_div8.textContent = localStorage.getItem("content8");
-    input_textarea8.value = localStorage.getItem("content8");
-  
-    function updateOutput8() {
-      localStorage.setItem("content8", input_textarea8.value);
-  
-      output_div8.textContent = input_textarea8.value;
-    }
-  }
-  
-  
-  function fivePM() {
-    var input_textarea9 = document.querySelector("#comment5pm");
-    var output_div9 = document.querySelector("#comment5pm");
-    var save_button9 = document.querySelector("#button5pm");
-  
-    save_button9.addEventListener("click", updateOutput9);
-  
-    output_div9.textContent = localStorage.getItem("content9");
-    input_textarea9.value = localStorage.getItem("content9");
-  
-    function updateOutput9() {
-      localStorage.setItem("content9", input_textarea9.value);
-  
-      output_div9.textContent = input_textarea9.value;
-    }
-  }
+
+
+  // display current day on page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+});
